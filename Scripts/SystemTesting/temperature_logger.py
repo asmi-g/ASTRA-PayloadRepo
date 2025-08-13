e2ecf47206c0f28517e3c449765a2832bdf9e04e
Asmi <asmi.gujral@gmail.com>
2025-08-13 19:41:58 -0400

    Changes made during integration day

--- Scripts/SystemTesting/temperature_logger.py
+++ Scripts/SystemTesting/temperature_logger.py
@@ -4,17 +4,30 @@
 # You should see something like /dev/i2c-1.
 
 import time
+import os
 import sys
 sys.path.append('/home/nvidia/.local/lib/python3.6/site-packages')
 
 import board
 import busio
-from Adafruit_MCP9808 import MCP9808
+# from Adafruit_MCP9808 import MCP9808
+import Adafruit_MCP9808.MCP9808 as MCP9808
 
-# Initialize I2C connection (Jetson TX2 usually uses I2C bus 1)
-i2c = busio.I2C(board.SCL, board.SDA)
-# Initialize MCP9808 sensor
-sensor = MCP9808.MCP9808(busnum=1)
+
+def find_mcp9808():
+  # Initialize I2C connection (Jetson TX2 usually uses I2C bus 1)
+  # i2c = busio.I2C(board.SCL_1, board.SDA_1)
+  try:
+    # Initialize MCP9808 sensor
+    print("Checking now")
+    sensor = MCP9808.MCP9808(busnum=1)
+    print("Called sensor")
+    sensor.begin()
+    print(sensor.readTempC())
+    return sensor
+  except (ValueError, OSError):
+    return None
+
 # Function to log temperature
 def log_temperature(log_file, interval=1):
   """
@@ -43,7 +56,12 @@ def log_temperature(log_file, interval=1):
 
 # Main function
 if __name__ == "__main__":
+  sensor = find_mcp9808()
+  if sensor is None:
+    print("No sensor detected.")
+  else:
+    print("Temp sensor detected!")
   timestamp = time.strftime("%Y-%m-%d_%H-%M-%S")
-  log_file_path = "Data/temperature/temperature_log_{}.csv".format(timestamp)
-  log_interval = 5  # Set logging interval in seconds
-  log_temperature(log_file_path, log_interval)
+  log_file_path = "/home/nvidia/Projects/ASTRA/ASTRA-GeneralRepo/Scripts/SDR/Data/temperature_log_{}.csv".format(timestamp)
+  # log_interval = 5  # Set logging interval in seconds
+  # log_temperature(log_file_path, log_interval)
